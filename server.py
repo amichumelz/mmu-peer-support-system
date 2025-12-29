d4125b6720fb7d535d64cfd9116491626874156f
RayyanSyahmi <167265000+RayyanSyahmi@users.noreply.github.com>
2025-12-29 19:02:37 +0800

    Update server.py
    added missing line 8

--- server.py
+++ server.py
@@ -2,18 +2,20 @@ import asyncio
 import websockets
 
 connected_clients = set()
-MAX = 2
 
-async def echo(websocket):
+
+async def handler(websocket):
+    connected_clients.add(websocket)
+    print("client joined")
     try:
         async for message in websocket:
             print(f"Received message from client: {message}")
-            await websocket.send(message)
     finally:
         connected_clients.remove(websocket)
+        print("client left")
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8765):
+    async with websockets.serve(handler, "localhost", 8765):
         print("Server started on ws://localhost:8765")
         await asyncio.Future()
 
